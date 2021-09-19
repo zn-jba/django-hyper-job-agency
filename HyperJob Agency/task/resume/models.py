acfe1fb81bce12e4a22e83526afba945f24d9b58
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-09-19 10:57:04 +0200

    create vacancy and resume models

--- HyperJob Agency/task/resume/models.py
+++ HyperJob Agency/task/resume/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Resume(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1024)
