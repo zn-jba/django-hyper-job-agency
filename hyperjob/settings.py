01029d1f50bad5b7c6e631c33d74be2fe6859b29
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-09-20 06:44:51 +0200

    remove redundant files

--- hyperjob/settings.py
+++ hyperjob/settings.py
@@ -121,5 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
